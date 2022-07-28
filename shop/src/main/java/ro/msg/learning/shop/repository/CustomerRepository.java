f683e77f0c65eca7cbecf14ef0812db3445120e5
Mircea Panaite <Mircea.Panaite@msg.group>
2022-07-28 13:16:36 +0300

    chapter03
    fixed Customer Repository

--- shop/src/main/java/ro/msg/learning/shop/repository/CustomerRepository.java
+++ shop/src/main/java/ro/msg/learning/shop/repository/CustomerRepository.java
@@ -2,7 +2,8 @@ package ro.msg.learning.shop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ro.msg.learning.shop.model.Customer;
 
 @Repository
-public interface Customer extends JpaRepository<Customer, Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
